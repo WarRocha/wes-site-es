24bd11518b941a5938ecb3e3a8dec606c177117a
warlei07 <warlei.rocha@digitalrepublic.com.br>
2025-02-12 15:47:16 -0300

    new site verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ export const metadata = {
   title: 'Aulas de espanhol online',
   description: 'Ofereço aulas de espanhol ao vivo e personalizadas, focadas nas suas necessidades',
   other: {
-    'google-site-verification': 'ERieQYkIjOEuZmSNmGnu9Czz2y_3mo2boN5WvoqQEmY',
+    'google-site-verification': 'AszpHctdDA5TCxs-Wfcl58cIZ9lNEkLKSoOWH3OB-KU',
   },
 }
 
