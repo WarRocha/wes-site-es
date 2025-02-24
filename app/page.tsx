08353090354f68f2c03ca44d0cf1bfea4aa53bd6
warlei07 <warlei.rocha@digitalrepublic.com.br>
2025-02-23 22:53:45 -0300

    fix pricee

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Dedicated from './components/Dedicated/index';
 import Manage from './components/Manage/index';
 import FAQ from './components/FAQ/index';
 import Quiz from './components/Quiz/index';
-import Testimonials from './components/Testimonials/index';
+// import Testimonials from './components/Testimonials/index';
 
 
 export default function Home() {
